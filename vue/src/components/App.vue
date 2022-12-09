7e4606dc5fda76a17a9b101688cf524dfb8cabc2
Hugo <hugoattal@hotmail.fr>
2022-12-09 16:01:26 +0100

    Add integration demo

---
+++ vue/src/components/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="app" ref="app">
+        App
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "App"
+};
+</script>
+
+<script setup lang="ts">
+
+import { LPFunctionEditor, loadApplication, loadNodeLib } from "@luna-park/integration";
+import { onMounted, ref } from "vue";
+import {LogicNodes as LogicNodesStandard} from "@luna-park/lib-standard"
+import {LogicNodes as LogicNodesString} from "@luna-park/lib-string"
+
+const app = ref();
+
+onMounted(() => {
+    loadApplication();
+    loadNodeLib("demo", LogicNodesStandard)
+    loadNodeLib("demo", LogicNodesString)
+    const editor = new LPFunctionEditor({ editorId: "demo", standalone: true });
+    app.value.append(editor);
+});
+</script>
+
+<style scoped lang="scss">
+.app {
+    height: 400px;
+}
+</style>
---
+++ vue/vite.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+/// <reference types="histoire" />
+
+import * as path from "path";
+import { defineConfig, UserConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+const alias = {
+    "@": path.resolve(__dirname, "src")
+};
+
+export default defineConfig(({ command }) => {
+    const config: UserConfig = {
+        plugins: [vue()],
+        resolve: {
+            alias
+        },
+        server: {
+            host: "127.0.0.1",
+            port: 2022
+        }
+    };
+
+    return config;
+});
